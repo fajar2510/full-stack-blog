119911d335062560b1a34155a24ddf75efbe518c
Fajar Abdurrohman <48176273+fajar2510@users.noreply.github.com>
2024-12-20 03:13:52 +0000

    MERN-stack-error-handling

--- backend/index.js
+++ backend/index.js
@@ -5,14 +5,25 @@ import postRouter from "./routes/post.route.js";
 import commentRouter from "./routes/comment.route.js";
 
 const app = express();
+app.use(express.json())
 
 // app.get("/test", (req, res) => {
 //   res.status(200).send("it's works!!");
 // });
 
-app.use("/users", userRouter);
+// app.use("/users", userRouter);
 app.use("/posts", postRouter);
-app.use("/comments", commentRouter);
+// app.use("/comments", commentRouter);
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500)
+
+  res.json({
+    message: error.message || "Somethng went wrong!",
+    status: error.status,
+    stack: error.stack,
+  });
+});
 
 app.listen(3000, () => {
   connectDB();
--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,9 +1,11 @@
 import express from "express";
+import { getPosts, getPost, createPost, deletePost } from "../controllers/post.controller.js";
 
 const router = express.Router();
 
-router.get("/post", (res, req) => {
-  res.status(200).send("Post Route");
-});
+router.get("/", getPosts); // get all post
+router.get("/:slug", getPost); // get single post
+router.post("/", createPost); // post single post
+router.delete("/:id", deletePost); // delete single post
 
 export default router;
